f31014a62fd65102cd1c4c8c67dcd303b6a4d4ea
YiZhiMCQiu <yizhimcqiu@icloud.com>
2025-09-22 13:12:19 +0800

    feat: 添加初始代码

---
+++ src/main/java/org/ceciliastudio/mython/Mython.java
@@ -0,0 +1,10 @@
+package org.ceciliastudio.mython;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mython implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/org/ceciliastudio/mython/client/MythonClient.java
@@ -0,0 +1,10 @@
+package org.ceciliastudio.mython.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MythonClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
